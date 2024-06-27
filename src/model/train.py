1fc5cb0c62a395d9f9333a121236b677affea5b0
ruthvikm <ruthviknathchowdary@gmail.com>
2024-06-27 18:33:36 -0500

    fixed lint issues

--- src/model/train.py
+++ src/model/train.py
@@ -8,18 +8,23 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+
 # define functions
 def main(args):
     # TO DO: enable autologging
     mlflow.sklearn.autolog()
+
     with mlflow.start_run():
         # read data
         df = get_csvs_df(args.training_data)
+
         # split data
         data = split_data(df)
         X_train, X_test, y_train, y_test = data['train']['X'], data['test']['X'], data['train']['y'], data['test']['y']
+
         # train model
         train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -28,6 +33,7 @@ def get_csvs_df(path):
     if not csv_files:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+
 
 # TO DO: add function to split data
 def split_data(df):
@@ -40,31 +46,41 @@ def split_data(df):
             "test": {"X": X_test, "y": y_test}}
     return data
 
+
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
+
 def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
+
     # add arguments
     parser.add_argument("--training_data", dest='training_data',
                         type=str)
     parser.add_argument("--reg_rate", dest='reg_rate',
                         type=float, default=0.01)
+
     # parse args
     args = parser.parse_args()
+
     # return args
     return args
+
+
 # run script
 if __name__ == "__main__":
     # add space in logs
     print("\n\n")
     print("*" * 60)
+
     # parse args
     args = parse_args()
+
     # run main function
     main(args)
+
     # add space in logs
     print("*" * 60)
     print("\n\n")
